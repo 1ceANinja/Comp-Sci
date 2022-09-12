e80fba888738bc5e159c723ec75005c60cc860d7
1ceANinja <113390900+1ceANinja@users.noreply.github.com>
2022-09-12 17:39:17 -0400

    Delete HW 1 Hello World!.py

--- HW 1 Hello World!.py
+++
@@ -1,7 +0,0 @@
-Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> #Name: Brian Boston
-... #Email: brian.boston61@myhunter.cuny.edu
-... #Date: September 11, 2022
-... #This program prints: Hello, World!
-... 
