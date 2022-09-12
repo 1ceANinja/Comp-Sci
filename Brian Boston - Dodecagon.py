e631258a6c94f2bcab5275d3b328f6f86cb8fab7
1ceANinja <113390900+1ceANinja@users.noreply.github.com>
2022-09-12 18:00:49 -0400

    Add files via upload

---
+++ Brian Boston - Dodecagon.py
@@ -0,0 +1,10 @@
+#Name: Brian Boston
+#Email: brian.boston61@myhunter.cuny.edu
+#Date: September 12, 2022
+#This program draws a dodecagon
+
+import turtle
+tommy = turtle.Turtle()
+for i in range(12):
+  tommy.forward(100)
+  tommy.left(30) 
